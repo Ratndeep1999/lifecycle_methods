874386856fd1d0e3a76a2772801541e9e3cb3cfd
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-28 15:51:35 +0530

    parent class and child class add

--- lib/did_update_widget.dart
+++ lib/did_update_widget.dart
@@ -11,6 +11,8 @@ class ParentWidget extends StatefulWidget {
 }
 
 class ParentWidgetState extends State<ParentWidget> {
+  bool isUpdateChild = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +20,70 @@ class ParentWidgetState extends State<ParentWidget> {
       body: SafeArea(
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              CustomButton(label: 'Change in Parent class', onPress: () {}),
+              /// Custom Button
+              CustomButton(
+                label: 'Change in Parent Widget Update Child State',
+                onPress: () {
+                  setState(() {
+                    isUpdateChild = true;
+                  });
+                },
+              ),
+
+              SizedBox(height: 40.0),
+
+              /// Child Widget
+              ChildWidget(isUpdateWidget: isUpdateChild),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+/// Child Widget Where Actually State Change
+class ChildWidget extends StatefulWidget {
+  final bool isUpdateWidget;
+
+  const ChildWidget({super.key, required this.isUpdateWidget});
+
+  @override
+  State<ChildWidget> createState() {
+    return ChildWidgetState();
+  }
+}
+
+/// Child Widget State
+class ChildWidgetState extends State<ChildWidget> {
+  late int index = 0;
+
+  // It initialize index
+  _initialize() {
+    index = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: [
+          Text("Counting : $index"),
+
+          SizedBox(height: 20),
+
+          // It Increase index
+          CustomButton(
+            label: 'Increase Count',
+            onPress: () {
+              setState(() {
+                index++;
+              });
+            },
+          ),
+        ],
       ),
     );
   }
