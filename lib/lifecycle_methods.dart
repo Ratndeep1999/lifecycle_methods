ed713c930a7263469596dcd4ff4f6dfc31a87865
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-28 10:41:33 +0530

    print statements add

--- lib/lifecycle_methods.dart
+++ lib/lifecycle_methods.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 class LifecycleMethods extends StatefulWidget {
-  const LifecycleMethods({Key? key}) : super(key: key);
+  LifecycleMethods({Key? key}) : super(key: key) {
+    debugPrint('Constructor called');
+  }
 
   @override
   State<LifecycleMethods> createState() {
+    debugPrint('createState() called');
     return LifecycleMethodsState();
   }
 }
@@ -12,42 +15,50 @@ class LifecycleMethods extends StatefulWidget {
 class LifecycleMethodsState extends State<LifecycleMethods> {
   @override
   void initState() {
+    debugPrint('initState() called');
     super.initState();
   }
 
   @override
   void didChangeDependencies() {
+    debugPrint('didChangeDependencies() called');
     super.didChangeDependencies();
   }
 
   @override
   void didUpdateWidget(LifecycleMethods oldWidget) {
+    debugPrint('didUpdateWidget called');
     super.didUpdateWidget(oldWidget);
   }
 
   @override
   void deactivate() {
+    debugPrint('deactivate() called');
     super.deactivate();
   }
 
   @override
   void dispose() {
+    debugPrint('dispose() called');
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: Text('Lifecycle Methods'), centerTitle: true),
-    body: SafeArea(
-      child: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            /// setState to call build()
-            setState(() {});
-          },
-          child: Text('set state'),
+  Widget build(BuildContext context) {
+    debugPrint('build() called');
+    return Scaffold(
+      appBar: AppBar(title: Text('Lifecycle Methods'), centerTitle: true),
+      body: SafeArea(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              /// setState to call build()
+              setState(() {});
+            },
+            child: Text('set state'),
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
