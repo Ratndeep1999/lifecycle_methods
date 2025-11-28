e17d3dbe2beef566a74ca85015de03cebd197f0e
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-28 15:03:32 +0530

    Parent class add for didUpdateWidget demo and one custom button also called

---
+++ lib/did_update_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'custom_button.dart';
+
+class ParentWidget extends StatefulWidget {
+  const ParentWidget({super.key});
+
+  @override
+  State<ParentWidget> createState() {
+    return ParentWidgetState();
+  }
+}
+
+class ParentWidgetState extends State<ParentWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('didUpdateWidget() Demo'), centerTitle: true),
+      body: SafeArea(
+        child: Center(
+          child: CustomButton(label: 'Change in Parent class', onPress: () {}),
+        ),
+      ),
+    );
+  }
+}
