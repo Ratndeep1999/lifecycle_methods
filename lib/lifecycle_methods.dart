2acb016176107d0fd05adfec0fa81dae6bd2793f
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-28 10:29:37 +0530

    lifecycle methods stateful class add

---
+++ lib/lifecycle_methods.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LifecycleMethods extends StatefulWidget {
+  const LifecycleMethods({Key? key}) : super(key: key);
+
+  @override
+  State<LifecycleMethods> createState() {
+    return LifecycleMethodsState();
+  }
+}
+
+class LifecycleMethodsState extends State<LifecycleMethods> {
+
+  @override
+  Widget build(BuildContext context) => Scaffold();
+}
