6ca951f33d9d6b20f17243093a7cda496446841a
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-28 10:29:16 +0530

    class called at home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lifecycle_methods/lifecycle_methods.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const ,
+      home: const LifecycleMethods(),
     );
   }
 }
